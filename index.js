8f60527a276691d3cc4a339214fa721b7ebe2170
Viktoria Braun <viktoria.braun@live.de>
2021-02-28 17:00:44 -0500

    implement first proof of concept

---
+++ index.js
@@ -0,0 +1,60 @@
+/** File: /index.js 
+ *  Desc: The entrypoint to the node application.
+ *  Authors: Viktoria Braun
+ *           Abbie Hurwitz
+ *           Tanjuma Haque
+*/
+
+// set up express app
+var express = require('express');
+var app = express();
+
+// set view engine to ejs
+app.set('view engine', 'ejs');
+
+// body parser
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({ extended : true }));
+
+// set up mongo client
+var MongoClient = require("mongodb").MongoClient;
+var uri = 'mongodb+srv://readonly:readonly@covid-19.hip2i.mongodb.net/covid19';
+
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+
+// get-data requests render a webpage with data from the database
+app.use('/get-data', (req, res) => {
+    var resultArray = [];
+
+    // connect to the mongo client
+    client.connect((err) => {
+        // extract data (currently hardcoded)
+        var data = client.db('covid19')
+                         .collection('global_and_us')
+                         .find({ country: 'Germany' })
+                         .sort(["date", -1])
+                         .limit(5);
+
+        // push the data objects into the results array
+        data.forEach((doc, err) => {
+            resultArray.push(doc);
+        }, () => {
+            client.close();
+            // render the data view
+            res.render('dataTable', {items: resultArray});
+        });
+    });
+});
+
+// home page
+app.use('/', (req, res) => {
+    res.send('Try going to /get-data!');
+});
+
+// listen for requests
+app.listen(3000, () => {
+    console.log('Listening on port 3000');
+});
---
+++ views/dataTable.ejs
@@ -0,0 +1,30 @@
+<!--  
+    File: views/dataTable.ejs 
+    Desc: The view that gets rendered in /get-data
+-->
+
+<!DOCTYPE html>
+<html>
+    <body>
+        <h1>Showing results for: <%= items[0].country %>.</h1>
+
+        <table>
+            <tr>
+                <td>Date</td>
+                <td><%= items[0].date %></td>
+            </tr>
+            <tr>
+                <td>Total Confirmed Cases</td>
+                <td><%= items[0].confirmed %></td>
+            </tr>
+            <tr>
+                <td>Total Deaths</td>
+                <td><%= items[0].deaths %></td>
+            </tr>
+            <tr>
+                <td>Total Recovered</td>
+                <td><%= items[0].recovered %></td>
+            </tr>
+        </table>
+    </body>
+</html>
