657a686243acaca02ce195fe9b95bd7d1b9263cd
abbiehurwitz <34584513+abbiehurwitz@users.noreply.github.com>
2021-04-19 14:43:54 -0400

    Update linegraph.js

--- public/scripts/linegraph.js
+++ public/scripts/linegraph.js
@@ -70,6 +70,8 @@ var svg = d3.select("#my_dataviz")
     .attr("transform",
           "translate(" + margin.left + "," + margin.top + ")");
 
+
+
 // Initialise a X axis:
 var x = d3.scaleTime().range([0,width]);
 var xAxis = d3.axisBottom().scale(x);
@@ -77,15 +79,25 @@ svg.append("g")
   .attr("transform", "translate(0," + height + ")")
   .attr("class","myXaxis")
 
+svg.append("text")
+  .attr("class", "x label")
+  .attr("text-anchor", "middle")
+  .attr("x", width/2)
+  .attr("y", height + 30)
+  .text("Date");
+
+
 // Initialize an Y axis
 var y = d3.scaleLinear().range([height, 0]);
 var yAxis = d3.axisLeft().scale(y);
 svg.append("g")
   .attr("class","myYaxis")
 
-  //data.forEach(function(d) {
-    //    d[0] = parseDate(d[0]);
-    //});
+  // Add brushing
+    var brush = d3.brushX()                   // Add the brush feature using the d3.brush function
+      .extent( [ [0,0], [width,height] ] )  // initialise the brush area: start at 0,0 and finishes at width,height: it means I select the whole graph area
+      //.on("end", updateChart)
+
 
   // Now I can use this dataset:
   function update(data) {
@@ -96,6 +108,7 @@ svg.append("g")
       .duration(3000)
       .call(xAxis);
 
+
     // update Y axis
     y.domain([0, d3.max(data, function(d) { return d.y  }) ])
     svg.selectAll(".myYaxis")
@@ -103,10 +116,23 @@ svg.append("g")
      .duration(3000)
      .call(yAxis);
 
+     var clip = svg.append("defs").append("svg:clipPath")
+           .attr("id", "clip")
+           .append("svg:rect")
+           .attr("width", width )
+           .attr("height", height )
+           .attr("x", 0)
+           .attr("y", 0);
+
+
+      var idleTimeout
+      function idled() { idleTimeout = null; }
+
 
      // Create a update selection: bind to the new data
       var u = svg.selectAll(".lineTest")
-        .data([data], function(d){ return d.x });
+      .attr("clip-path", "url(#clip)")
+      .data([data], function(d){ return d.x });
 
       // Updata the line
       u
@@ -123,7 +149,55 @@ svg.append("g")
           .attr("stroke", "steelblue")
           .attr("stroke-width", 2.5)
 
+          // Add the brushing
+          u
+              .append("g")
+                .attr("class", "brush")
+                .call(brush);
 
-    }
+       // A function that set idleTimeOut to null
+       svg.on("dblclick",function(){
+         console.log("this happened");
+         x.domain(d3.extent(data, function(d) { return d.x; }))
+         xAxis.transition().call(d3.axisBottom(x))
+         line
+           .select('.u')
+           .transition()
+           .attr("d", d3.line()
+             .x(function(d) { return x(d.x) })
+             .y(function(d) { return y(d.y) })
+         )
+       });
+
+       function updateChart() {
+
+         // What are the selected boundaries?
+         extent = d3.event.selection
+
+         // If no selection, back to initial coordinate. Otherwise, update X axis domain
+         if(!extent){
+           if (!idleTimeout) return idleTimeout = setTimeout(idled, 350); // This allows to wait a little bit
+           x.domain([ 4,8])
+         }else{
+           x.domain([ x.invert(extent[0]), x.invert(extent[1]) ])
+           line.select(".brush").call(brush.move, null) // This remove the grey brush area as soon as the selection has been done
+         }
+
+         // Update axis and line position
+         xAxis.transition().duration(1000).call(d3.axisBottom(x))
+         line
+             .select('.u')
+             .transition()
+             .duration(1000)
+             .attr("d", d3.line()
+               .x(function(d) { return x(d.x) })
+               .y(function(d) { return y(d.y) })
+             )
+       }
+
+    };
+    // A function that update the chart for given boundaries
+
+    // If user double click, reinitialize the chart
 
 update(bestdata1)
