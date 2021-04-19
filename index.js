97c7155e6e98de776d707120f98675f227d37f6b
Viktoria Braun <vcbraun@brynmawr.edu>
2021-04-19 17:56:46 -0400

    change port

--- index.js
+++ index.js
@@ -8,7 +8,7 @@
 // set up express app
 var express = require('express');
 var app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const path = require('path');
 const util = require('util');
 
