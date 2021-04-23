c94335a18db91af205c5e6b8dfe3cede6327abf7
tanjuma <pinnochio420@gmail.com>
2021-04-23 17:16:02 -0400

    bug fixes & top ten
    -- reduced unnecessary styling as well

--- public/scripts/stateBarchart.js
+++ public/scripts/stateBarchart.js
@@ -41,43 +41,45 @@ window.onload = function(){
   };
 var d = "confirmedData";
 var perabs ="absolute";
+var highesttenbtn = "";
+
 var s1 = JSON.parse(JSON.stringify(confirmedData));
 var s2 = JSON.parse(JSON.stringify(deathData));
 var s3 = JSON.parse(JSON.stringify(percentCon));
 var s4 = JSON.parse(JSON.stringify(percentDeath));
 
-var ascendingS = document.createElement("button");
-var ascendingV = document.createElement("button");
+var descendingS = document.createElement("button");
+var descendingV = document.createElement("button");
 
-ascendingS.setAttribute("class","smallbtn");
-ascendingV.setAttribute("class","smallbtn");
+descendingS.setAttribute("class","smallbtn");
+descendingV.setAttribute("class","smallbtn");
 
-ascendingS.innerHTML = "Ascending Order: By County";
-ascendingV.innerHTML = "Ascending Order: By Value";
+descendingS.innerHTML = "Descending Order: By County";
+descendingV.innerHTML = "Descending Order: By Value";
 
 
 // 2. Append to Container
 var graphDiv = document.getElementById("barchartContainer")
-graphDiv.appendChild(ascendingS);
-graphDiv.appendChild(ascendingV);
+graphDiv.appendChild(descendingS);
+graphDiv.appendChild(descendingV);
 
 // 3. Add event handlers
 function percentAb(val){
   perabs = val;
   if(val == "absolute"){
     if(d == "confirmedData"){
-      update(confirmedData);
+      barupdate(confirmedData);
     }
     else{
-      update(deathData);
+      barupdate(deathData);
     }
   }
   else{
     if(d == "confirmedData"){
-      update(percentCon);
+      barupdate(percentCon);
     }
     else{
-      update(percentDeath);
+      barupdate(percentDeath);
     }
   }
 }
@@ -86,20 +88,20 @@ function formdisplay(val){
     document.getElementById('heading').innerHTML = "Confirmed Cases";
     d = "confirmedData";
     if(perabs == "absolute"){
-      update(confirmedData);
+      barupdate(confirmedData);
     }
     else{
-      update(percentCon);
+      barupdate(percentCon);
     }
   }
   else{
     document.getElementById('heading').innerHTML = "Deaths";
     d = "deathData";
     if(perabs == "absolute"){
-      update(deathData);
+      barupdate(deathData);
     }
     else{
-      update(percentDeath);
+      barupdate(percentDeath);
     }
   }
 }
@@ -107,16 +109,18 @@ function sortandupdatebyCounty(dat){
   dat.sort(function (a, b) {
   return a.County.localeCompare(b.County);
   });
-  update(dat);
+  barupdate(dat);
 }
 
 function sortandupdatebyValue(dat){
   dat.sort(function (a, b) {
-  return parseFloat(a.Value) - parseFloat(b.Value);
+  return parseFloat(b.Value) - parseFloat(a.Value);
   });
-  update(dat);
+  if (highesttenbtn !== "clicked"){
+    barupdate(dat);
+  }
 }
-ascendingS.addEventListener ("click", function() {
+descendingS.addEventListener ("click", function() {
   if(d == "confirmedData"){
     if(perabs == "absolute"){
       sortandupdatebyCounty(s1);
@@ -135,7 +139,7 @@ ascendingS.addEventListener ("click", function() {
   }
 });
 
-ascendingV.addEventListener ("click", function() {
+descendingV.addEventListener ("click", function() {
   if(d == "confirmedData"){
     if(perabs == "absolute"){
       sortandupdatebyValue(s1);
@@ -154,20 +158,70 @@ ascendingV.addEventListener ("click", function() {
   }
 });
 
+var highestArr = [];
+function highestten(){
+  highesttenbtn = "clicked";
+  if(d == "confirmedData"){
+    if(perabs == "absolute"){
+      sortandupdatebyValue(s1);
+      highestArr = [];
+      for (i in s1){
+        if (i < 10){
+          highestArr.push(s1[i]);
+        }
+      }
+      barupdate(highestArr)
+    }
+    else{
+      sortandupdatebyValue(s3);
+      highestArr = [];
+      for (i in s3){
+        if (i < 10){
+          highestArr.push(s3[i]);
+        }
+      }
+      barupdate(highestArr)
+    }
+  }
+  else{
+    if(perabs == "absolute"){
+      sortandupdatebyValue(s2);
+      highestArr = [];
+      for (i in s2){
+        if (i < 10){
+          highestArr.push(s2[i]);
+        }
+      }
+      barupdate(highestArr)
+    }
+    else{
+      sortandupdatebyValue(s4);
+      highestArr = [];
+      for (i in s4){
+        if (i < 10){
+          highestArr.push(s4[i]);
+        }
+      }
+      barupdate(highestArr)
+    }
+  }
+  highesttenbtn = "";
+}
+
 // set the dimensions and margins of the graph
 var w = (window.screen.width)*.95;
-var margin = {top: 10, right: 30, bottom: 70, left: 60},
-    width = w - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
+var barmargin = {top: 10, right: 30, bottom: 70, left: 60},
+    width = w - barmargin.left - barmargin.right,
+    height = 400 - barmargin.top - barmargin.bottom;
 
 // append the svg object to the body of the page
-var svg = d3.select("#chart")
+var barsvg = d3.select("#chart")
   .append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
+    .attr("width", width + barmargin.left + barmargin.right)
+    .attr("height", height + barmargin.top + barmargin.bottom)
   .append("g")
     .attr("transform",
-          "translate(" + margin.left + "," + margin.top + ")");
+          "translate(" + barmargin.left + "," + barmargin.top + ")");
 
 var toolTipDiv = d3.select("body").append("div")
     .style("opacity", 0)
@@ -180,35 +234,35 @@ var toolTipDiv = d3.select("body").append("div")
 
 
 // Initialize the X axis
-var x = d3.scaleBand()
+var barx = d3.scaleBand()
   .range([ 0, width ])
   .padding(0.2);
-var xAxis = svg.append("g")
+var barxAxis = barsvg.append("g")
   .attr("transform", "translate(0," + height + ")")
 
 // Initialize the Y axis
-var y = d3.scaleLinear()
+var bary = d3.scaleLinear()
   .range([ height, 0]);
-var yAxis = svg.append("g")
+var baryAxis = barsvg.append("g")
   .attr("class", "myYaxis")
 
 
 // A function that create / update the plot for a given variable:
-function update(data) {
+function barupdate(data) {
 
   // Update the X axis
-  x.domain(data.map(function(d) { return d.County; }))
-  xAxis.call(d3.axisBottom(x))
+  barx.domain(data.map(function(d) { return d.County; }))
+  barxAxis.call(d3.axisBottom(barx))
   .selectAll("text")
     .attr("transform", "translate(-10,0)rotate(-35)")
     .style("text-anchor", "end");
 
   // Update the Y axis
-  y.domain([0, d3.max(data, function(d) { return d.Value }) ]);
-  yAxis.transition().duration(1000).call(d3.axisLeft(y));
+  bary.domain([0, d3.max(data, function(d) { return d.Value }) ]);
+  baryAxis.transition().duration(1000).call(d3.axisLeft(bary));
 
   // Create the u variable
-  var u = svg.selectAll("rect")
+  var baru = barsvg.selectAll("rect")
     .data(data)
 
   function onMouseOver(d){
@@ -244,24 +298,24 @@ function onMouseOut(d){
       .style("opacity", 1)
 }
 
-  u
+  baru
     .enter()
     .append("rect") // Add a new rect for each new elements
     .on("mouseover", onMouseOver)
     .on("mouseout", onMouseOut)
-    .merge(u) // get the already existing elements as well
+    .merge(baru) // get the already existing elements as well
     .transition() // and apply changes to all of them
     .duration(1000)
-      .attr("x", function(d) { return x(d.County); })
-      .attr("y", function(d) { return y(d.Value); })
-      .attr("width", x.bandwidth())
-      .attr("height", function(d) { return height - y(d.Value); })
+      .attr("x", function(d) { return barx(d.County); })
+      .attr("y", function(d) { return bary(d.Value); })
+      .attr("width", barx.bandwidth())
+      .attr("height", function(d) { return height - bary(d.Value); })
       .attr("fill", "#69b3a2")
 
   // If less State in the new dataset, I delete the ones not in use anymore
-  u
+  baru
     .exit()
     .remove()
 }
   document.getElementById('heading').innerHTML = "Confirmed Cases";
-  update(confirmedData);
+  barupdate(confirmedData);
